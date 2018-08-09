5bd7016bdd3fc4fa895d3a6ca81d03f4c524b67c
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-09 12:28:53 +0200

    msm8952: fix registry_table name
    The registy_table name does not match the source name
    and the build will fail.
    
    Test: manual - Build OK
    
    Change-Id: I2789555066ff3787ca1b797f4eff75c284930896
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -78,7 +78,7 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8952 msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table_android.c
 else
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table_android.c
@@ -103,7 +103,7 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8952 msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table.c
 else
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table.c
