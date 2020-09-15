14ff9bc2b0895f15cb85e249ef4dd4c972a1f64f
Marijn Suijten <marijns95@gmail.com>
2020-09-16 01:14:42 +0200

    Remove COPY_HEADERS
    Nothing seems to depend on these being "exported", and copying headers
    is disallowed starting from Android R.
    
    Signed-off-by: Marijn Suijten <marijns95@gmail.com>

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -17,14 +17,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-#===============================================================================
-#             Deploy the headers that can be exposed
-#===============================================================================
-
-LOCAL_COPY_HEADERS_TO   := mm-core/omxcore
-LOCAL_COPY_HEADERS      := QComOMXMetadata.h \
-                           QComOMXPlugin.h
-
 LOCAL_SRC_FILES := \
     QComOMXPlugin.cpp                      \
 
@@ -55,4 +47,3 @@ LOCAL_MODULE := libstagefrighthw
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
-
